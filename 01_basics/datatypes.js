8368b4c4c78b3543704e002289d5cb1c86fca095
Abhilash Bag <102401165+abhilashbag@users.noreply.github.com>
2024-01-13 18:30:38 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict" ; // treat all js code as newer version
+
+//alert('hello');
+
+
+let name = "Abhi"
+let age = 90
+let isLoggedIn = false
+
+// PRIMITIVE DATATYPES
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean = true/false
+//null => standalone value(representation of empty)
+//undefined =>  Not assign value to variable
+//symbol =>mostly for react
+
+//object
+console.log(typeof null); 
+//Undifined
+console.log(typeof undefined); 
+
+
